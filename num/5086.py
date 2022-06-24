3680fa7a1d572bb396d64776b72a4fa6784e5ac1
jungjh404 <jungjh404@gmail.com>
2022-06-25 00:38:02 +0900

    num2

---
+++ num/5086.py
@@ -0,0 +1,11 @@
+import sys
+
+N, M = map(int, sys.stdin.readline().split())
+while N != 0 and M != 0:
+    if N % M == 0:
+        print("multiple")
+    elif M%N == 0:
+        print("factor")
+    else:
+        print("neither")
+    N, M = map(int, sys.stdin.readline().split())
